adb992f16edc02ba7c47889b064512c541e15a3a
MigCap <miguelangel.capellan@vass.es>
2020-05-09 22:03:56 +0200

    - Added initial form to get coffee values.

--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 const textInputDecoration = InputDecoration(
   fillColor: Colors.white,
-  focusColor: Colors.brown,
-  // filled: true,
+  filled: true,
   contentPadding: EdgeInsets.all(12.0),
-  // enabledBorder: OutlineInputBorder(
-  //   borderSide: BorderSide(color: Colors.white, width: 2.0),
-  // ),
-  // focusedBorder: OutlineInputBorder(
-  //   borderSide: BorderSide(color: Colors.brown, width: 2.0),
-  // ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 2.0),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.pink, width: 2.0),
+  ),
 );
