dc300b1c11fe9c80d1127eb03c1e6e7e78b600ff
MigCap <miguelangel.capellan@vass.es>
2020-05-09 19:41:13 +0200

    - Few things: - Firebase configuration. - Sign In. Auht. - Register. - Set Theme.

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -14,12 +14,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-  String error = '';
   bool loading = false;
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +33,14 @@ class _RegisterState extends State<Register> {
               title: Text('Sign up to Brew Crew'),
               actions: <Widget>[
                 FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text('Sign In'),
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onPressed: () => widget.toggleView(),
                 ),
               ],
@@ -43,23 +49,27 @@ class _RegisterState extends State<Register> {
               padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
               child: Form(
                 key: _formKey,
-                child: Center(
+                child: SingleChildScrollView(
                   child: Column(
                     children: <Widget>[
                       SizedBox(height: 20.0),
                       TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Email'),
+                        decoration: textInputDecoration.copyWith(
+                          icon: Icon(Icons.alternate_email),
+                          labelText: "Email",
+                        ),
                         validator: (val) =>
                             val.isEmpty ? 'Enter an email' : null,
                         onChanged: (val) {
                           setState(() => email = val);
                         },
                       ),
-                      SizedBox(height: 20.0),
+                      SizedBox(height: 10.0),
                       TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Password'),
+                        decoration: textInputDecoration.copyWith(
+                          icon: Icon(Icons.lock),
+                          labelText: "Password",
+                        ),
                         obscureText: true,
                         validator: (val) => val.length < 6
                             ? 'Enter a password 6+ chars long'
@@ -69,26 +79,29 @@ class _RegisterState extends State<Register> {
                         },
                       ),
                       SizedBox(height: 20.0),
-                      RaisedButton(
-                          color: Colors.brown[400],
-                          child: Text(
-                            'Register',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              setState(() => loading = true);
-                              dynamic result =
-                                  await _auth.registerWithEmailAndPassword(
-                                      email, password);
-                              if (result == null) {
-                                setState(() {
-                                  loading = false;
-                                  error = 'Please supply a valid email';
-                                });
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                            color: Colors.brown[400],
+                            child: Text(
+                              'Register',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                setState(() => loading = true);
+                                dynamic result =
+                                    await _auth.registerWithEmailAndPassword(
+                                        email, password);
+                                if (result == null) {
+                                  setState(() {
+                                    loading = false;
+                                    error = 'Please supply a valid email';
+                                  });
+                                }
                               }
-                            }
-                          }),
+                            }),
+                      ),
                       SizedBox(height: 12.0),
                       Text(
                         error,
--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 const textInputDecoration = InputDecoration(
   fillColor: Colors.white,
-  filled: true,
+  focusColor: Colors.brown,
+  // filled: true,
   contentPadding: EdgeInsets.all(12.0),
-  enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.brown, width: 2.0),
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.brown, width: 2.0),
-  ),
+  // enabledBorder: OutlineInputBorder(
+  //   borderSide: BorderSide(color: Colors.white, width: 2.0),
+  // ),
+  // focusedBorder: OutlineInputBorder(
+  //   borderSide: BorderSide(color: Colors.brown, width: 2.0),
+  // ),
 );
